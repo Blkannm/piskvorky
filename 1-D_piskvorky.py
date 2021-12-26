b630f581f0607c8aef8c09687b2fe9275ce1e6d3
blkannm <trung.le3301@gmail.com>
2021-12-26 20:56:49 +0100

    zmena 3

--- 1-D_piskvorky.py
+++ 1-D_piskvorky.py
@@ -6,25 +6,26 @@ def tah(pole, cislo_policka, symbol):
     pole = (zacatek_pole+ symbol + konec_pole)
     return pole
 
-
 def tah_hrace():
-    while():
-        pozice = int(input("kam chces hrat?"))
-        if pozice < 0:
-            print("nelze zadat zaporne cislo")
-        elif pozice >= 0:
-            break
-        else:
-            print("nerozumim zkus to znova")
-    return (pozice)
+    pozice = input("kam chces hrat? ")
+    return pozice
 
 def Id_piskvorky():
+    pole = 20*"-"
+    while(pole.find("-") >= 0 ):
+        cislo_pole = tah_hrace()
+        if pole[cislo_pole] == "-":
+            pole = tah(pole,cislo_pole,"x")
+            print(pole)
+        elif pole[cislo_pole] != "-":
+            print("zabrane pole!")
     
-    print()
-    return
+    #pole = tah(pole, 8,"x")
+    #tah_pocitace = randrange(0, 19)
+    #print(tah_pocitace)
+    #pole = tah(pole, tah_pocitace, "o")
+    
+    return print(pole)
 
 Id_piskvorky()
 
-
-# zmnena
-# zmena 2 t
